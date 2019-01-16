c7bd0a8f153d85f5cdb99cd950a24322a7cd1cab
issenn <issenn@hellotalk.com>
2019-01-16 16:27:19 +0800

    ...

--- Formula/curl-max.rb
+++ Formula/curl-max.rb
@@ -69,7 +69,7 @@ class CurlMax < Formula
       --disable-debug
       --disable-dependency-tracking
       --disable-silent-rules
-      --prefix=#{prefix}
+      --prefix=taki-kun-#{prefix}
     ]
 
     # cURL has a new firm desire to find ssl with PKG_CONFIG_PATH instead of using
