67ffdfed31a2efe2aec900a810423f695eb7d420
issenn <issenn@hellotalk.com>
2019-02-15 14:25:17 +0800

    Initial commit

---
+++ Formula/sdat2img.rb
@@ -0,0 +1,11 @@
+class Sdat2img < Formula
+    desc "Convert sparse Android data image (.dat) into filesystem ext4 image (.img)"
+    homepage "https://github.com/xpirt/sdat2img"
+    url "https://raw.githubusercontent.com/xpirt/sdat2img/master/sdat2img.py"
+    version "0.0.1"
+    sha256 "35c39baf6f7e700e4cbebf70b86d602e312e3146e58ec1e8b08c1baf046edeb9"
+  
+    def install
+      bin.install "sdat2img.py"
+    end
+  end
