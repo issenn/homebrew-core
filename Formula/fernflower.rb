d2ac861096d9b19e6fab48db5453a2b684b503e8
issenn <issenn@issenn.com>
2019-04-15 19:17:20 +0800

    update fernflower

--- Formula/fernflower.rb
+++ Formula/fernflower.rb
@@ -1,16 +1,14 @@
 class Fernflower < Formula
+  desc "First actually working analytical decompiler for Java"
   homepage "https://github.com/fesh0r/fernflower"
-
-  head do
-    url "https://github.com/fesh0r/fernflower.git"
-    depends_on "gradle" => :build
-  end
+  url "https://github.com/fesh0r/fernflower.git", :using => :git
+  version "0.0.1"
 
   depends_on :java => "1.7+"
 
   def install
-    system "gradle", "build"
-    libexec.install Dir["build/libs/fernflower.jar"]
+    system "./gradlew", "build"
+    libexec.install "build/libs/fernflower.jar"
     bin.write_jar_script libexec/"fernflower.jar", "fernflower"
   end
 
