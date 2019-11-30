bac8e0f02eba7d5dcf5c1342b0c8bb19815ce37a
issenn <issennknight@gmail.com>
2019-11-30 08:24:18 +0800

    Initial commit

--- Formula/baidupcs-go.rb
+++ Formula/baidupcs-go.rb
@@ -2,7 +2,7 @@ class BaidupcsGo < Formula
   desc "仿 Linux shell 文件处理命令的百度网盘命令行客户端"
   homepage "https://github.com/iikira/BaiduPCS-Go"
 
-  url "https://github.com/iikira/BaiduPCS-Go/releases/download/v3.5.6/BaiduPCS-Go-v3.6.1-darwin-osx-amd64.zip"
+  url "https://github.com/iikira/BaiduPCS-Go/releases/download/v3.6.1/BaiduPCS-Go-v3.6.1-darwin-osx-amd64.zip"
   version "3.6.1"
   sha256 "db087e64d606639ca3e05a4b482174a06d3c0eabd6cbdbcc317374fc40742a96"
 
