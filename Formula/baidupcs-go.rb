b500a0ddbaa22735ba400e11342c08e3137afd0c
issenn <issennknight@gmail.com>
2020-02-06 00:22:48 +0800

    Initial commit

--- Formula/baidupcs-go.rb
+++ Formula/baidupcs-go.rb
@@ -2,7 +2,7 @@ class BaidupcsGo < Formula
   desc "The terminal utility for Baidu Network Disk"
   homepage "https://github.com/iikira/BaiduPCS-Go"
   version "3.6.1"
-  url "https://github.com/iikira/BaiduPCS-Go/archive/#{version}.tar.gz"
+  url "https://github.com/iikira/BaiduPCS-Go/archive/v#{version}.tar.gz"
   sha256 "07e1d802e6b49425a53b883c354d8b753de55f7005ac8c7b27cd8396db0447da"
 
   # bottle do
